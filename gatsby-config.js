c0a05e9916facd9396cac77d6378095fe7f545f6
Fred <73175901+diverdowngit@users.noreply.github.com>
2023-02-02 13:48:05 +0100

    first

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,6 +15,7 @@ module.exports = {
     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
   },
   plugins: [
+    `gatsby-plugin-netlify`,
     `gatsby-plugin-image`,
     {
       resolve: `gatsby-source-filesystem`,
@@ -39,5 +40,9 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    
+     "gatsby-plugin-netlify"
+    
+    
   ],
 }
