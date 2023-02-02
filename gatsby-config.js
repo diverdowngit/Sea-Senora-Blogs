3422f5b5d109860daa92dcee052ead0b9bba24fe
Fred <73175901+diverdowngit@users.noreply.github.com>
2023-02-02 15:11:51 +0100

    first

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,7 +11,7 @@ module.exports = {
   siteMetadata: {
     title: `SV Sea Senora`,
     description: `WebPage for SV Sea Senora. Sailing,Travel,YouTube And Blog Site.A Must To Check Out.`,
-    author: `@gatsbyjs`,
+    author: `fred`,
     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
   },
   plugins: [
@@ -26,20 +26,20 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
+    //{
+      // resolve: `gatsby-plugin-manifest`,
+      // options: {
+      //   name: `gatsby-starter-default`,
+      //   short_name: `starter`,
+      //   start_url: `/`,
+      //   background_color: `#663399`,
         // This will impact how browsers show your PWA/website
         // https://css-tricks.com/meta-theme-color-and-trickery/
         // theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
-    },
+        // display: `minimal-ui`,
+        // icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+    //  },
+   // },
     
      "gatsby-plugin-netlify"
     
